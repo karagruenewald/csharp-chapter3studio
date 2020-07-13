b29fed835df93f5cd83490736475098a6113bb87
Kara Gruenewald <karakilkenny@karas-imac.attlocal.net>
2020-07-13 11:50:22 -0500

    completed

---
+++ Chapter3Studio/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chapter3Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string test = System.IO.File.ReadAllText(@"/Users/karakilkenny/codecamp2020/c-sharp/csharp-studios/Chapter3Studio/Chapter3Studio/TextFile.txt");
+            CountingCharacters(test);
+
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+            CountingCharacters(input);
+        }
+
+
+        static void CountingCharacters(string paragraph)
+        {
+            paragraph = paragraph.ToLower();
+            Dictionary<char, int> newDictionary = new Dictionary<char, int>();
+            char[] charactersInString = paragraph.ToCharArray();
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            for (int i = 0; i < charactersInString.Length; i++)
+            {
+                if (!newDictionary.ContainsKey(charactersInString[i]) && alphabet.Contains(charactersInString[i]))
+                {
+                    int n = 0;
+                    foreach (char item in charactersInString)
+                    {
+                        if (item == charactersInString[i])
+                        {
+                            n++;
+                        }
+                    }
+                    newDictionary.Add(charactersInString[i], n);
+                }
+            }
+            foreach (KeyValuePair<char, int> item in newDictionary)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
